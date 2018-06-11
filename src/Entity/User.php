4f0c916ffa110c716ff0a6a22a944d3d153332e2
laurentBsss <danone97200@gmail.com>
2018-06-12 00:24:08 +0200

    create user entity

---
+++ src/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}
