9ef0c8ad7d1402998589d06f5d474b3770a8fe7e
laurentBsss <danone97200@gmail.com>
2018-06-29 22:16:04 +0200

    add vichuploder bundle and admin upload

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -3,9 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ * @Vich\Uploadable
  */
 class Post
 {
@@ -25,6 +28,18 @@ class Post
      * @ORM\Column(type="text", nullable=true)
      */
     private $content;
+
+   /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="post_image", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;   
 
     /**
      * @ORM\Column(type="datetime")
@@ -72,7 +87,7 @@ class Post
 
     public function setCreatedAt(\DateTimeInterface $created_at): self
     {
-        $this->created_at = $created_at;
+        $this->created_at = new \DateTime('now');
 
         return $this;
     }
@@ -88,4 +103,34 @@ class Post
 
         return $this;
     }
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    
 }
